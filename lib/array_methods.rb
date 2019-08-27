09bff65022a5956e635993cd7122616ae52d447d
Austin Smith <trunk9slamchest@gmail.com>
2019-08-27 23:40:07 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,19 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  (array.length).times do |e|
+    if array[e] == value_to_find
+      return e
+    end
+  end
+
+  return nil
 end
 
 def find_max_value(array)
-  # Add your solution here
+  sorted = array.sort
+  return sorted[-1]
 end
 
 def find_min_value(array)
-  # Add your solution here
+  sorted = array.sort
+  return sorted[0]
 end
