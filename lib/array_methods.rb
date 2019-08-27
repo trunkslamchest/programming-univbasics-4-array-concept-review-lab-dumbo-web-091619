b99b3997081468e20045fc7a39cdfcb925bdd5ea
Austin Smith <trunk9slamchest@gmail.com>
2019-08-27 23:39:56 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,3 @@
-  # scale = [2,3,4,5]
-  # scale_2 = [6,5,4,3]
-  # hill = [1,2,3,4,5,4,3,2,1]
-  # valley = [5,4,3,2,1,0,1,2,3,4,5]
-  # zig_zag = [500,4,1000,5,250]
-
 def find_element_index(array, value_to_find)
   (array.length).times do |e|
     if array[e] == value_to_find
@@ -23,15 +17,3 @@ def find_min_value(array)
   sorted = array.sort
   return sorted[0]
 end
-
-# find_max_value(scale)
-# find_max_value(scale_2)
-# find_max_value(hill)
-# find_max_value(valley)
-# find_max_value(zig_zag)
-
-# find_min_value(scale)
-# find_min_value(scale_2)
-# find_min_value(hill)
-# find_min_value(valley)
-# find_min_value(zig_zag)
