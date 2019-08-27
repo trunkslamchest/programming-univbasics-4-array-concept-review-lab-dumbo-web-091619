5e3bbdccc257ea854bc7c9ccc264c296f398c193
Austin Smith <trunk9slamchest@gmail.com>
2019-08-27 23:31:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,4 +1,4 @@
-  # scale = [2,3,4,5]
+  scale = [2,3,4,5]
   # scale_2 = [6,5,4,3]
   # hill = [1,2,3,4,5,4,3,2,1]
   # valley = [5,4,3,2,1,0,1,2,3,4,5]
@@ -24,6 +24,6 @@ end
 
 
 # find_element_index(scale,2)
-# find_element_index(scale,4)
+find_element_index(scale,4)
 # find_element_index(scale_2,5)
 # find_element_index(scale_2,6)
