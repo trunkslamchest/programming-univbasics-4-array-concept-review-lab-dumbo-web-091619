e57e0abc8914769244928590c56ef398acc2e948
Austin Smith <trunk9slamchest@gmail.com>
2019-08-27 23:33:18 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,10 +10,9 @@ def find_element_index(array, value_to_find)
       if array[e] == value_to_find
         puts e
       return e
-  end
-    # else
-    #   return nil
-    # end
+    else
+      return nil
+    end
   end
 end
 
