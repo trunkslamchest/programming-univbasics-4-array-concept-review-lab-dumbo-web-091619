a1730b1a92f9e16e72f6b20eff0978a9060cfbc5
Austin Smith <trunk9slamchest@gmail.com>
2019-08-27 23:26:26 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,3 +1,9 @@
+  scale = [2,3,4,5]
+  scale_2 = [6,5,4,3]
+  hill = [1,2,3,4,5,4,3,2,1]
+  valley = [5,4,3,2,1,0,1,2,3,4,5]
+  zig_zag = [500,4,1000,5,250]
+
 def find_element_index(array, value_to_find)
   # Add your solution here
 end
